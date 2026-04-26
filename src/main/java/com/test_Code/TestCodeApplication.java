17d9a5685b309f1a10159789d6b295f3d3e04249
Manoj-kr <mroul914@gmail.com>
2026-04-26 16:49:28 +0530

    add sopln

---
+++ src/main/java/com/test_Code/TestCodeApplication.java
@@ -0,0 +1,14 @@
+package com.test_Code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCodeApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
---
+++ src/test/java/com/test_Code/TestCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test_Code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
